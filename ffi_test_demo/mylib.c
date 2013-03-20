8cd64896e7bd0acd9615edb1109649f4e0195c76
U-AWIFI-GISLAB\GISLAB-Mark <GISLAB-Mark@AWIFI-GISLAB.(none)>
2013-03-20 16:47:45 +0800

    sendbox/ffi_demo

---
+++ ffi_test_demo/mylib.c
@@ -0,0 +1,5 @@
+// clang -shared mylib.c -o libmylib.so
+
+int plus(int x, int y){ return x + y; };
+
+//正常的去使用C的寫法就可以了
---
+++ ffi_test_demo/ruby.rb
@@ -0,0 +1,13 @@
+
+require 'ffi'
+
+module MyLib
+  extend FFI::Library
+  ffi_lib './libmylib.so'
+  attach_function :plus, [:int ,:int], :int
+end
+
+p MyLib.plus(1, 2)
+
+#透過模組化將C func叫出來
+#然後再透過ruby語法給叫出來執行
