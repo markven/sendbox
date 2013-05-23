80eebbd0d90c22954ceb058f24059fc57ab3abba
U-AWIFI-GISLAB\GISLAB-Mark <GISLAB-Mark@AWIFI-GISLAB.(none)>
2013-05-23 17:08:16 +0800

    modify_ruby_test

--- ruby_test/ruby_test.rb
+++ ruby_test/ruby_test.rb
@@ -19,6 +19,37 @@ open('map_sv2.jpg', 'wb') do |f|
     f.close()
 end
 
+def GetStreetView(panoid_i,zoom)
+
+	basew = 512
+	baseh = 512
+    matchw = 96
+    matchh = 48
+    x = 2 ** zoom - matchw * (2 ** zoom) / basew
+    y = (2 ** zoom - 1) - (matchh * (2 ** zoom) / baseh)
+    width = basew * (2 ** zoom) - matchw * (2 ** zoom)
+    height = baseh * (2 ** zoom - 1) - matchh * (2 ** zoom)
+
+
+	mosaic_pana = ImageList.new
+	mosaic_pana.new_image(width, height)
+
+	for i in 1 .. y
+		for j in 1 .. x
+			mosaic_temp = ImageList.new
+			mosaic_temp.read "http://cbk1.google.com/cbk?output=tile&panoid="+panoid_i.to_s+"&zoom="+zoom.to_s+"&x="+j.to_s+"&y="+i.to_s+"&cb_client=maps_sv"
+			mosaic_pana.composite!(mosaic_temp, j*basew , i*baseh , CopyCompositeOp)
+		end
+	end
+
+	mosaic_pana.write("pana_"+zoom.to_s+"_"+panoid_i+".jpg")
+
+end
+
+GetStreetView("4fe6hEN9GJC6thoQBcgv0Q",2)
+
+
+=begin
 
 cat = ImageList.new('map_sv2.jpg')
 cat3 = ImageList.new
@@ -32,6 +63,10 @@ mosaic.composite!(cat2,0,0,CopyCompositeOp)
 #cat3.display
 cat2.write("animated.jpg")
 cat3.write("animated2.jpg")
-mosaic.write("animated_m.jpg")
+mosaic.write("animated_m.jpg")	
+
+	
+=end
+
 
 # puts open('http://cbk1.google.com/cbk?output=tile&panoid=4fe6hEN9GJC6thoQBcgv0Q&zoom=4&x=10&y=2&cb_client=maps_sv').read
