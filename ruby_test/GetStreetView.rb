3b3e8b2aef1f13bcfbd1bc22e5550656d2001813
U-AWIFI-GISLAB\GISLAB-Mark <GISLAB-Mark@AWIFI-GISLAB.(none)>
2013-05-23 18:08:29 +0800

    modify_GetStreetView

---
+++ ruby_test/GetStreetView.rb
@@ -0,0 +1,29 @@
+require 'RMagick'
+include Magick
+
+def GetStreetView(panoid_i,zoom)
+
+	basew = 512
+	baseh = 512
+    matchw = 96
+    matchh = 48
+    x = (2 ** zoom) - (matchw*(2 ** zoom)/basew)
+    y = ((2 ** zoom)-1) - (matchh * (2 ** zoom)/baseh)
+    width = basew * (2 ** zoom) - matchw * (2 ** zoom)
+    height = baseh * (2 ** (zoom-1)) - matchh * (2 ** zoom)
+
+
+	mosaic_pana = ImageList.new
+	mosaic_pana.new_image(width, height)
+
+	for i in 0 .. y.ceil
+		for j in 0 .. x.ceil
+			mosaic_temp = ImageList.new
+			mosaic_temp.read "http://cbk1.google.com/cbk?output=tile&panoid="+panoid_i+"&zoom="+zoom.to_s+"&x="+j.to_s+"&y="+i.to_s+"&cb_client=maps_sv"
+			mosaic_pana.composite!(mosaic_temp, j*basew , i*baseh , CopyCompositeOp)
+		end
+	end
+
+	mosaic_pana.write("pana_"+zoom.to_s+"_"+panoid_i+".jpg")
+
+end
--- ruby_test/ruby_test.rb
+++ ruby_test/ruby_test.rb
@@ -1,6 +1,7 @@
 require 'open-uri' 
 require 'net/http'
 require 'RMagick'
+require './GetStreetView.rb' 
 include Magick
 
 # 開啟一個二進制檔案，將檔案物件放入 f 這個變數內
@@ -19,35 +20,9 @@ open('map_sv2.jpg', 'wb') do |f|
     f.close()
 end
 
-def GetStreetView(panoid_i,zoom)
-
-	basew = 512
-	baseh = 512
-    matchw = 96
-    matchh = 48
-    x = 2 ** zoom - matchw * (2 ** zoom) / basew
-    y = (2 ** zoom - 1) - (matchh * (2 ** zoom) / baseh)
-    width = basew * (2 ** zoom) - matchw * (2 ** zoom)
-    height = baseh * (2 ** zoom - 1) - matchh * (2 ** zoom)
-
-
-	mosaic_pana = ImageList.new
-	mosaic_pana.new_image(width, height)
-
-	for i in 1 .. y
-		for j in 1 .. x
-			mosaic_temp = ImageList.new
-			mosaic_temp.read "http://cbk1.google.com/cbk?output=tile&panoid="+panoid_i.to_s+"&zoom="+zoom.to_s+"&x="+j.to_s+"&y="+i.to_s+"&cb_client=maps_sv"
-			mosaic_pana.composite!(mosaic_temp, j*basew , i*baseh , CopyCompositeOp)
-		end
-	end
-
-	mosaic_pana.write("pana_"+zoom.to_s+"_"+panoid_i+".jpg")
-
+for i in 1 .. 2
+	GetStreetView("4fe6hEN9GJC6thoQBcgv0Q",i)
 end
-
-GetStreetView("4fe6hEN9GJC6thoQBcgv0Q",2)
-
 
 =begin
 
